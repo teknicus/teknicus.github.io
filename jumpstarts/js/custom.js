b63c7d20f004cf4935b715661d2afbeeb6aa2783
“teknicus” <mailme@thomasrob.in>
2020-04-15 16:22:11 +0530

    JumpStarts init

---
+++ jumpstarts/js/custom.js
@@ -0,0 +1,4 @@
+const targetElement = document.querySelector('#someElementId');
+ 
+// 2. ...in some event handler after showing the target element...disable body scroll
+bodyScrollLock.disableBodyScroll(targetElement);
