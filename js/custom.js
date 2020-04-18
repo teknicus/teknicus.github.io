610cdc1b1b88eefe9b15d319d637265a44b3890d
“teknicus” <mailme@thomasrob.in>
2020-04-18 12:28:35 +0530

    Fixed JS errors

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,1 @@
 console.log("loaded");
-
-const targetElement = document.querySelector('#someElementId');
- 
-// 2. ...in some event handler after showing the target element...disable body scroll
-bodyScrollLock.disableBodyScroll(targetElement);
